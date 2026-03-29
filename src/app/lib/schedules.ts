0eef03a7cbe7a34edfe35f75f6b0d6676298aaa1
Brady Price <bradyprice1@gmail.com>
2026-03-29 21:47:41 +0000

    add all 162 games of the Orioles

--- src/app/lib/schedules.ts
+++ src/app/lib/schedules.ts
@@ -124,6 +124,7 @@ export const sportsData: Sport[] = [
       ...generateMLBGames("MIA", "Miami Marlins", "mlb-26-mia"),
       ...generateMLBGames("MIL", "Milwaukee Brewers", "mlb-26-mil"),
       ...generateMLBGames("CWS", "Chicago White Sox", "mlb-26-cws"),
+      ...generateMLBGames("BAL", "Baltimore Orioles", "mlb-26-bal"),
       { id: 'mlb-26-asg', name: "2026 MLB All-Star Game (Atlanta)", date: "Jul 14, 2026" },
       { id: 'mlb-26-ws', name: "2026 World Series: Game 1", date: "Oct 23, 2026" },
     ], 
